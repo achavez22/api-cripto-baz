89de26c631916ad93318e2b67aa61fc9ecdfe97c
Adan Chavez OLivera <adanch22@gmail.com>
2023-11-23 22:33:04 -0600

    first commit

---
+++ src/main/java/com/operpagos/apicriptobaz/ApiCriptoBazApplication.java
@@ -0,0 +1,13 @@
+package com.operpagos.apicriptobaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCriptoBazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCriptoBazApplication.class, args);
+    }
+
+}
